eee2acb01860f0c3e592edcb94879f1421c34af5
HanMin <94442043+HaloTwo@users.noreply.github.com>
2024-11-09 19:19:43 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        cout << (int)s.find(c) << " ";
+    }
+
+    return 0;
+}
