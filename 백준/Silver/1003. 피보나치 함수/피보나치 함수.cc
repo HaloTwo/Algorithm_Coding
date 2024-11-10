df3d71f8377934acb516deb48b0960d27ad4ead7
HanMin <94442043+HaloTwo@users.noreply.github.com>
2024-11-10 16:09:48 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -2,31 +2,31 @@
 #include <vector>
 using namespace std;
 
+
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
-    int T;
-    cin >> T;
+	vector<pair<int, int>> dp(40);
 
-    // dp[i] = {0의 호출 횟수, 1의 호출 횟수}
-    vector<pair<int, int>> dp(41);
-    dp[0] = { 1,0 };  // N=0일 때: 0은 1번, 1은 0번
-    dp[1] = { 0,1 };  // N=1일 때: 0은 0번, 1은 1번
+	dp[0] = { 1, 0 };
+	dp[1] = { 0, 1 };
 
-    // 점화식으로 각 N에 대한 0,1 호출 횟수 계산
-    for (int i = 2; i <= 40; i++) 
-    {
-        dp[i].first = dp[i - 1].first + dp[i - 2].first;   // 0 호출 횟수
-        dp[i].second = dp[i - 1].second + dp[i - 2].second; // 1 호출 횟수
-    }
+	for (int i = 2; i <= 40; i++)
+	{
+		dp[i].first = dp[i - 1].first + dp[i - 2].first;  
+		dp[i].second = dp[i - 1].second + dp[i - 2].second;
+	}
 
-    while (T--) 
-    {
-        int N;
-        cin >> N;
-        cout << dp[N].first << ' ' << dp[N].second << '\n';
-    }
+	int T, N;
 
-    return 0;
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N;
+		cout << dp[N].first << " " << dp[N].second << "\n";
+	}
+
+	return 0;
 }
