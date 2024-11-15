e91a22e55bd1e7517d515808b71c5435808acf07
HanMin <94442043+HaloTwo@users.noreply.github.com>
2024-11-16 00:11:31 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	long long int x, y;
+
+	cin >> x >> y;
+	cout << abs(x - y);
+
+
+	return 0;
+}
