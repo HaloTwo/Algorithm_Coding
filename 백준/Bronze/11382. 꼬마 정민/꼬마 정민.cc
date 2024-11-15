53d5c0f2437ee1a5d0755f76e84b1f96ef1b589d
HanMin <94442043+HaloTwo@users.noreply.github.com>
2024-11-16 00:08:39 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	long long int x, y, z;
+
+
+	cin >> x >> y >> z;
+	cout << x + y + z;
+
+
+	return 0;
+}
