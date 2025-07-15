a3bca6fa2b55911bacf248a9056532b490aa87c4
SuminLee <lsm6265@naver.com>
2025-07-15 20:47:24 +0900

    [Silver I] Title: 배열 돌리기, Time: 204 ms, Memory: 4020 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.cc
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.cc
@@ -1,75 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-vector<vector<int>> arr;
-int s = 0;
-int degree = 0;
-
-void Rotate(int dir)
-{
-	vector<vector<int>>temp = arr;
-	int num = s / 2;
-	int tmp = s-1;
-	for (int i = 0; i < s; i++)
-	{
-		if (dir > 0)
-		{
-			temp[i][num] = arr[i][i]; // 주 대각선을 가운데 열로
-			temp[i][tmp] = arr[i][num]; // 가운데 열을 부 대각선으로
-			temp[num][i] = arr[tmp][i]; // 부 대각선을 가운데 행으로
-			temp[i][i] = arr[num][i]; // 가운데 행을 주 대각선으로
-		}
-		else
-		{
-			temp[num][i] = arr[i][i]; // 주 대각선을 가운데 행으로
-			temp[i][i] = arr[i][num]; // 가운데 열을 주 대각선으로
-			temp[i][num] = arr[i][tmp]; // 부 대각선을 가운데 열로
-			temp[tmp][i] = arr[num][i]; // 가운데 행을 부 대각선으로
-		}
-		tmp--;
-	}
-	arr = temp;
-}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int t = 0;
-	cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	while (t--)
-	{
-		cin >> s >> degree;
-		
-		int dir = 1;
-		if (degree < 0)
-		{
-			dir = -1;
-			degree *= -1;
-		}
-		degree /= 45;
-		arr.clear();
-		arr.resize(s, vector<int>(s));
-		for (int i = 0; i < s; i++)
-		{
-			for (int j = 0; j < s; j++)
-			{
-				cin >> arr[i][j];
-			}
-		}
-		while (degree--)
-		{
-			Rotate(dir);
-		}
-		for (int i = 0; i < s; i++)
-		{
-			for (int j = 0; j < s; j++)
-			{
-				cout << arr[i][j]<<" ";
-			}
-			cout << '\n';
-		}
-		
-	}
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, d;
+        cin >> n >> d;
 
-	return 0;
+        vector<vector<int>> board(n, vector<int>(n));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> board[i][j];
+
+        int step = ((d / 45) % 8 + 8) % 8;
+        int mid = n / 2;
+
+        for (int s = 0; s < step; s++) {
+            vector<vector<int>> temp = board;
+            for (int i = 0; i < n; i++) {
+                temp[i][mid] = board[i][i];
+                temp[i][n - 1 - i] = board[i][mid];
+                temp[mid][n - 1 - i] = board[i][n - 1 - i];
+                temp[i][i] = board[mid][i];
+            }
+            board = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                cout << board[i][j] << " ";
+            cout << "\n";
+        }
+    }
+    return 0;
 }
