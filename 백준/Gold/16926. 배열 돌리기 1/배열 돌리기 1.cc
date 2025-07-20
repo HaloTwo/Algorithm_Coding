cdc87f282a05e150d2ea4d88fc4b1252396854e2
SuminLee <lsm6265@naver.com>
2025-07-20 15:22:42 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 20 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.cc
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.cc
@@ -1,92 +1,77 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
     int N, M, R;
     cin >> N >> M >> R;
 
-    vector<vector<int>> vec(N, vector<int>(M));
-
+    vector<vector<int>> map(N, vector<int>(M));
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            cin >> vec[i][j];
+            cin >> map[i][j];
         }
     }
 
     int layer = min(N, M) / 2;
-    vector<vector<int>> layerVec(layer);
 
-    // 1. 레이어별로 테두리 뽑기
     for (int i = 0; i < layer; i++)
     {
-        // 윗줄
-        for (int x = i; x < M - i; x++)
-            layerVec[i].push_back(vec[i][x]);
+        vector<int> line;
 
-        // 오른쪽
-        for (int y = i + 1; y < N - i; y++)
-            layerVec[i].push_back(vec[y][M - i - 1]);
+        // 상단 가로줄
+        for (int j = i; j < M - i - 1; j++)
+            line.push_back(map[i][j]);
 
-        // 아랫줄
-        for (int x = M - i - 2; x >= i; x--)
-            layerVec[i].push_back(vec[N - i - 1][x]);
+        // 오른쪽 세로줄
+        for (int j = i; j < N - i - 1; j++)
+            line.push_back(map[j][M - i - 1]);
 
-        // 왼쪽
-        for (int y = N - i - 2; y > i; y--)
-            layerVec[i].push_back(vec[y][i]);
-    }
+        // 하단 가로줄
+        for (int j = M - i - 1; j > i; j--) // ✅ M-i-2까지
+            line.push_back(map[N - i - 1][j]);
 
-    // 2. 회전시키기
-    for (int i = 0; i < layer; i++)
-    {
-        int len = layerVec[i].size();
-        int rot = R % len;
+        // 왼쪽 세로줄
+        for (int j = N - i - 1; j > i; j--) // ✅ N-i-2까지
+            line.push_back(map[j][i]);
 
-        for (int r = 0; r < rot; r++)
-        {
-            int temp = layerVec[i].front();
-            layerVec[i].erase(layerVec[i].begin());
-            layerVec[i].push_back(temp);
-        }
-    }
+        int rotateSize = R % line.size();
+        rotate(line.begin(), line.begin() + rotateSize, line.end());
 
-    // 3. 원래 배열에 다시 넣기
-    for (int i = 0; i < layer; i++)
-    {
         int idx = 0;
 
-        // 윗줄
-        for (int x = i; x < M - i; x++)
-            vec[i][x] = layerVec[i][idx++];
+        // 상단 가로줄
+        for (int j = i; j < M - i - 1; j++)
+            map[i][j] = line[idx++];
 
-        // 오른쪽
-        for (int y = i + 1; y < N - i; y++)
-            vec[y][M - i - 1] = layerVec[i][idx++];
+        // 오른쪽 세로줄
+        for (int j = i; j < N - i - 1; j++)
+            map[j][M - i - 1] = line[idx++];
 
-        // 아랫줄
-        for (int x = M - i - 2; x >= i; x--)
-            vec[N - i - 1][x] = layerVec[i][idx++];
+        // 하단 가로줄
+        for (int j = M - i - 1; j > i; j--)
+            map[N - i - 1][j] = line[idx++];
 
-        // 왼쪽
-        for (int y = N - i - 2; y > i; y--)
-            vec[y][i] = layerVec[i][idx++];
+        // 왼쪽 세로줄
+        for (int j = N - i - 1; j > i; j--)
+            map[j][i] = line[idx++];
     }
 
-    // 4. 출력
+    // 출력
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            cout << vec[i][j] << " ";
+            cout << map[i][j] << " ";
         }
         cout << "\n";
     }
