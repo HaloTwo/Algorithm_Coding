7819b04d7ae368fc016eaf5940133cfd37aacc4d
SuminLee <lsm6265@naver.com>
2025-07-20 14:33:10 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,65 +1,82 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
+
 	int N, M;
-	vector<vector<char>> vec;
 
 	cin >> N >> M;
 
-	vec.resize(N, vector<char>(M));
+	vector<vector<char>> map(N, vector<char>(M));
 
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
 		{
-			cin >> vec[i][j];
+			cin >> map[i][j];
 		}
 	}
 
-	int answer = 64; // 최대 8x8 다 바꾸는 경우
+	int minCount = 64;
 
 	for (int i = 0; i <= N - 8; i++)
 	{
 		for (int j = 0; j <= M - 8; j++)
 		{
-			int w_count = 0; // W로 시작한다고 가정
-			int b_count = 0; // B로 시작한다고 가정
+			int wStart = 0;
+			int bStart = 0;
 
 			for (int x = 0; x < 8; x++)
 			{
 				for (int y = 0; y < 8; y++)
 				{
-					char current = vec[i + x][j + y];
+					char current = map[i + x][j + y];
 
-					// 짝수 칸
+					//짝수일 경우
 					if ((x + y) % 2 == 0)
 					{
-						if (current != 'W') w_count++;
-						if (current != 'B') b_count++;
+						
+						if (current != 'W')
+						{
+							wStart++;
+						}
+						else
+						{
+							bStart++;
+						}
+
+
 					}
-					// 홀수 칸
+					//홀수일 경우
 					else
 					{
-						if (current != 'B') w_count++;
-						if (current != 'W') b_count++;
+						if (current != 'W')
+						{
+							bStart++;
+						}
+						else
+						{
+							wStart++;
+						}
 					}
+
 				}
 			}
 
-			int curr_min = min(w_count, b_count);
-			answer = min(answer, curr_min);
+			minCount = min(minCount, min(bStart, wStart));
 		}
 	}
 
-	cout << answer;
+	cout << minCount;
+
 
 	return 0;
 }
